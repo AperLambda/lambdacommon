69f9fc07cf768d4cb89c272be6a090a6aeffaed8
LambdAurora <aurora42lambda@gmail.com>
2018-12-29 22:57:17 +0100

    :construction_worker: Fixing Linux with clang CI build.

--- tests/travis_setup_linux.sh
+++ tests/travis_setup_linux.sh
@@ -5,8 +5,3 @@ sudo apt-get install -y -qq libyajl-dev libxml2-dev libxqilla-dev
 if [ "$CXX" = "clang++" ]; then
     sudo apt-get install -y  -qq libc++-dev;
 fi
-if [ "$CXX" = "g++" ]; then
-    echo "Installing GCC..."
-    sudo apt-get install -y -qq g++-7
-    export CXX="g++-7" CC="gcc-7"
-fi
