83ec9aaa1e84b55485f78721f356ca946551bfaf
LambdAurora <aurora42lambda@gmail.com>
2018-12-29 21:22:06 +0100

    :construction_worker: Fixing Travis build...

--- tests/travis_setup_linux.sh
+++ tests/travis_setup_linux.sh
@@ -2,7 +2,9 @@
 echo "Installing compile dependencies..."
 sudo apt-get update -y -qq
 sudo apt-get install -y -qq libyajl-dev libxml2-dev libxqilla-dev
-if [ "$CXX" = "clang++" ]; then sudo apt-get install -y  -qq libstdc++-6-dev; fi
+if [ "$CXX" = "clang++" ]; then
+    sudo apt-get install -y  -qq libc++-dev;
+fi
 if [ "$CXX" = "g++" ]; then
     echo "Installing GCC..."
     sudo apt-get install -y -qq g++-7
